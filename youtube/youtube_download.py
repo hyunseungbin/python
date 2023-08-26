51d34707ea3561bf601a08320868aa71f87e845a
hyunseungbeen <tmdqls6625@naver.com>
2023-08-26 09:31:48 +0900

    youtube downloader

--- youtube/youtube_download.py
+++ youtube/youtube_download.py
@@ -1,6 +1,14 @@
 from urllib.request import Request, urlopen
 import json
 import pprint
+
+def find_list(listdata, key, value):
+    for item in listdata:
+        if item.get(key) == value:
+            return item
+    return None
+
+
 header = {
     "user-agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
     "referer" : "https://www.youtube.com"
@@ -21,5 +29,14 @@ if start_index < end_index:
     
     h = watch_html[start_index + len(parse):end_index+1]
     json_data = json.loads(h)
-    with open("python\youtube\youtube_data.txt","wt",encoding="utf-8") as out:
-        pprint.pprint(json_data,stream=out)
+    
+    aformats = json_data["streamingData"]["adaptiveFormats"]
+    data = next((item for item in aformats if item["itag"] == 137), None)
+    if data is not None:
+        sample_url = data["url"]
+        quaulity = data["quality"]
+        request = Request(sample_url, headers=header)
+        
+        r = urlopen(request)
+        file_size = r.headers['Content-Length']
+        print(r.headers)
